564be386a5113661c3a463924f4992435de2c4fd
EC2 Default User <ec2-user@ip-172-31-23-62.ec2.internal>
2021-09-08 15:16:26 +0000

    Initial Commit

---
+++ app/models/book.rb
@@ -0,0 +1,25 @@
+class Book < ApplicationRecord
+  belongs_to :user
+	has_many :favorites, dependent: :destroy
+	has_many :book_comments, dependent: :destroy
+  validates :title, presence: true
+  validates :body, presence: true, length: { maximum: 200 }
+  validates :rate, presence: true, numericality: {
+    less_than_or_equal_to: 5,
+    greater_than_or_equal_to: 1
+  }
+
+	def favorited_by?(user)
+		favorites.where(user_id: user.id).exists?
+	end
+
+	def Book.book_search(keyword)
+    Book.where("category LIKE?", "%#{keyword}%")
+  end
+
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) }
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day)}
+  scope :created_last_week, -> { where(created_at: 13.day.ago.beginning_of_day..1.week.ago.end_of_day)}
+
+end
